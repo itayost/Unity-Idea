103be6fe1ec26202ab511de9a07320dffd7aa011
itayost <itayost1@gmail.com>
2024-07-07 14:54:04 +0300

    as

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,62 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Numerics;
 using UnityEngine;
 
-public class PlayerBehaviour : MonoBehaviour
+namespace playerNameSpace
 {
-    public GameObject aCamera;
-    CharacterController controller;
-    float speed = 4;
-    float angularSpeed = 50;
-    //AudioSource footStepSnd;
-
-    public static bool canMove = true;
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerBehaviour : MonoBehaviour
     {
-        controller = GetComponent<CharacterController>(); // connects controller to player's controller
-        //footStepSnd = GetComponent<AudioSource>();
-    }
+        public GameObject aCamera;
+        CharacterController controller;
+        float speed = 4;
+        float angularSpeed = 50;
+        //AudioSource footStepSnd;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (canMove)
+        public static bool canMove = true;
+        // Start is called before the first frame update
+        void Start()
         {
-            float dx, dz;
-            float rotationAboutY, rotationAboutX;
+            controller = GetComponent<CharacterController>(); // connects controller to player's controller
+                                                              //footStepSnd = GetComponent<AudioSource>();
+        }
 
-            // simple motion
-            // transform.Translate(new Vector3(0,0,0.01f));
-
-            rotationAboutX = -Input.GetAxis("Mouse Y") * angularSpeed * Time.deltaTime;
-            rotationAboutY = Input.GetAxis("Mouse X") * angularSpeed * Time.deltaTime;
-            transform.Rotate(new UnityEngine.Vector3(0, rotationAboutY, 0));
-            // rotates only camera
-            aCamera.transform.Rotate(new UnityEngine.Vector3(rotationAboutX, 0, 0));
-
-
-            dx = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-            dz = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-
-            UnityEngine.Vector3 motion = new UnityEngine.Vector3(dx, -1, dz); // In Local coordinates
-
-            motion = transform.TransformDirection(motion); // transforms local coordinates to global coordinates
-
-            controller.Move(motion); // in global coordinates
-            if (dx != 0 || dz != 0)
+        // Update is called once per frame
+        void Update()
+        {
+            if (canMove)
             {
-                /*if (!footStepSnd.isPlaying)
+                float dx, dz;
+                float rotationAboutY, rotationAboutX;
+
+                // simple motion
+                // transform.Translate(new Vector3(0,0,0.01f));
+
+                rotationAboutX = -Input.GetAxis("Mouse Y") * angularSpeed * Time.deltaTime;
+                rotationAboutY = Input.GetAxis("Mouse X") * angularSpeed * Time.deltaTime;
+                transform.Rotate(new UnityEngine.Vector3(0, rotationAboutY, 0));
+                // rotates only camera
+                aCamera.transform.Rotate(new UnityEngine.Vector3(rotationAboutX, 0, 0));
+
+
+                dx = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+                dz = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+                UnityEngine.Vector3 motion = new UnityEngine.Vector3(dx, -1, dz); // In Local coordinates
+
+                motion = transform.TransformDirection(motion); // transforms local coordinates to global coordinates
+
+                controller.Move(motion); // in global coordinates
+                if (dx != 0 || dz != 0)
                 {
-                    footStepSnd.Play();
-                }*/
+                    /*if (!footStepSnd.isPlaying)
+                    {
+                        footStepSnd.Play();
+                    }*/
+                }
+            }
+
+        }
+
+        public void UpdateSpeed(float additionalSpeed)
+        {
+            if(speed + additionalSpeed > 0)
+            {
+                speed += additionalSpeed;
             }
         }
 
-    }
-    public static void setCanMove(bool flag)
-    {
-        canMove = flag;
+        public static void setCanMove(bool flag)
+        {
+            canMove = flag;
+        }
     }
 }
--- Assets/Scripts/SurpisesBehaviour.cs
+++ Assets/Scripts/SurpisesBehaviour.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using playerNameSpace;
+using System;
+using System.Numerics;
 
 public class SurpisesBehaviour : MonoBehaviour
 {
@@ -10,11 +13,19 @@ public class SurpisesBehaviour : MonoBehaviour
     //public Text keysText;
     public GameObject player;
     public bool exists = true;
+    public float minX, maxX, minZ, maxZ; // Boundaries of the table
+    public float yPosition = (float)-9.187248;
+    public int speed;
     // Start is called before the first frame update
     void Start()
     {
         if (!exists)
             gameObject.SetActive(false); // checks if key is visible.
+
+        minX = -5;
+        maxX = 16;
+        minZ = -14;
+        maxZ = 27;
     }
 
     // Update is called once per frame
@@ -30,10 +41,22 @@ public class SurpisesBehaviour : MonoBehaviour
             //keysText.text = "Keys: " + numKeys + "/4";
             exists = false;
             //PersistentObjectManager.Instance.setHasCoin(false);
-            gameObject.SetActive(false);
+            playerNameSpace.PlayerBehaviour player1 = player.GetComponent<playerNameSpace.PlayerBehaviour>();
+            player1.UpdateSpeed(speed);
+            RandomizePosition();
             //AudioSource sound = keys.GetComponent<AudioSource>();
             //sound.Play();
         }
+    }
+
+    private void RandomizePosition()
+    {
+        float randomX = UnityEngine.Random.Range(minX, maxX);
+        float randomZ = UnityEngine.Random.Range(minZ, maxZ);
+
+        UnityEngine.Debug.Log("x,y,z: " + randomX + ", " + yPosition + ", " + randomZ);
+        UnityEngine.Vector3 newPos = new UnityEngine.Vector3(randomX, yPosition, randomZ);
+        transform.localPosition = newPos;
     }
 
     public static int getNumOfKeys()
